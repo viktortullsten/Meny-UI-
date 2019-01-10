9a026c609c0e7023824f51dc88b960fd916c0232
Viktor Tullsten <viktor.tullsten@elev.ga.lbs.se>
2019-01-10 11:02:58 +0100

    Meny UI

---
+++ Assets/ButtenToggler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtenToggler : MonoBehaviour
+{
+
+
+    public void ToggleChanged(bool state)
+    {
+        GetComponent<Button>().interactable = state;
+    }
+}
+
+
---
+++ Assets/rotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+
+	public void Rotation(float degrees)
+    {
+        transform.Rotate(0, 0, degrees);
+    }
+   
+}
